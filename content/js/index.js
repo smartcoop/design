ef3f9fbc9b988dcd86ef68b1ac0d83ca02165889
Fanny Benisek <fb@mono.company>
2021-09-23 15:28:04 +0200

    Add global banner

--- content/js/index.js
+++ content/js/index.js
@@ -6,3 +6,4 @@ import "./modules/tabs";
 import "./modules/input-password.js"
 import "./modules/popper-tooltip";
 import "./modules/design-system-website-mobile-menu";
+import "./modules/close-global-banner";
