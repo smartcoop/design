10861edf0fc4342d04bf9c9127426af141ded460
FannyBenisek <FannyBenisek@users.noreply.github.com>
2021-09-24 11:34:15 +0000

    deploy: a1350dfa66fb243044fcc5331115ef559c0cd6a3

--- js/bundle-client.js
+++ js/bundle-client.js
@@ -26,17 +26,41 @@ require("./modules/navbar-components");
 
 /* Accordion
    ========================================================================== */
-$('.c-accordion__item-header').on('click', function () {
-  $(this).closest('.c-accordion__item').toggleClass('c-accordion__item--closed');
+var headersAcc = document.querySelectorAll('.c-accordion .c-accordion__item .c-accordion__item-header');
 
-  if ($(this).attr('aria-expanded') === 'false') {
-    $(this).attr('aria-expanded', 'true');
-    $(this).siblings(".c-accordion__item-content").attr('aria-hidden', 'false');
+for (var i = 0; i < headersAcc.length; i++) {
+  headersAcc[i].addEventListener('click', openCurrAcc);
+} //Use this as the callback if you would like multiple dropdowns to be open
+
+
+function openAcc(e) {
+  var parent = this.parentElement;
+  var article = this.nextElementSibling;
+
+  if (!parent.classList.contains('js-active')) {
+    parent.classList.add('js-active');
+    article.style.maxHeight = article.scrollHeight + 'px';
   } else {
-    $(this).attr('aria-expanded', 'false');
-    $(this).siblings(".c-accordion__item-content").attr('aria-hidden', 'true');
+    parent.classList.remove('js-active');
+    article.style.maxHeight = '0px';
   }
-});
+} //Use this as the callback if you would like no more than one dropdown to be open
+
+
+function openCurrAcc(e) {
+  for (var i = 0; i < headersAcc.length; i++) {
+    var parent = headersAcc[i].parentElement;
+    var article = headersAcc[i].nextElementSibling;
+
+    if (this === headersAcc[i] && !parent.classList.contains('js-active')) {
+      parent.classList.add('js-active');
+      article.style.maxHeight = article.scrollHeight + 'px';
+    } else {
+      parent.classList.remove('js-active');
+      article.style.maxHeight = '0px';
+    }
+  }
+}
 
 },{}],3:[function(require,module,exports){
 "use strict";
