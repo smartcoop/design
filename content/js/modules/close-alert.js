cf52d37bdc134c048c6cf390f77dcf34e09b1c3e
Emily <evdw@mono.company>
2021-10-07 17:27:31 +0200

    Clean up alert/unused classes
    #88

---
+++ content/js/modules/close-alert.js
@@ -0,0 +1,23 @@
+/* alert JS
+   ========================================================================== */
+
+   class CloseAlert {
+    constructor(alert) {
+        this.alertClass = alert;
+        this.closeButton = alert.querySelector('[data-alert-close]');
+        this.init();
+    }
+
+    init() {
+      const parentClassScope = this;
+      this.closeButton.addEventListener('click', function(e){
+        parentClassScope.alertClass.remove();
+      });
+    }
+  }
+
+  const closableAlerts = document.querySelectorAll('[data-alert-closable]');
+
+  if (closableAlerts.length) {
+    [...closableAlerts].map((closableAlertsWithClick) => new CloseAlert(closableAlertsWithClick));
+  }
