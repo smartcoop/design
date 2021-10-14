c2d6a73b4245b7e1aff23cb809909af761f66566
Johan Ronsse <johan.ronsse@gmail.com>
2021-10-14 10:37:09 +0200

    Improve docs/indentation js files

--- content/js/modules/accordion.js
+++ content/js/modules/accordion.js
@@ -1,40 +1,40 @@
 /* Accordion
    ========================================================================== */
 
-   var headersAcc = document.querySelectorAll('.c-accordion .c-accordion__item .c-accordion__item-header');
+var headersAcc = document.querySelectorAll('.c-accordion .c-accordion__item .c-accordion__item-header');
 
-   for(var i = 0; i < headersAcc.length; i++) {
-    headersAcc[i].addEventListener('click', openCurrAcc);
-   }
-   
-   //Use this as the callback if you would like multiple dropdowns to be open
-   function openAcc(e) {
-     var parent = this.parentElement;
-     var article = this.nextElementSibling;
-     
-     if (!parent.classList.contains('js-active')) {
-       parent.classList.add('js-active');
-       article.style.maxHeight = article.scrollHeight + 'px';
-     }
-     else {
-       parent.classList.remove('js-active');
-       article.style.maxHeight = '0px';
-     }
-   }
-   
-   //Use this as the callback if you would like no more than one dropdown to be open
-   function openCurrAcc(e) {
-     for(var i = 0; i < headersAcc.length; i++) {
-       var parent = headersAcc[i].parentElement;
-       var article = headersAcc[i].nextElementSibling;
-   
-       if (this === headersAcc[i] && !parent.classList.contains('js-active')) {
-         parent.classList.add('js-active');
-         article.style.maxHeight = article.scrollHeight + 'px';
-       }
-       else {
-         parent.classList.remove('js-active');
-         article.style.maxHeight = '0px';
-       }
-     }
-   }
+for(var i = 0; i < headersAcc.length; i++) {
+  headersAcc[i].addEventListener('click', openCurrAcc);
+}
+
+//Use this as the callback if you would like multiple dropdowns to be open
+function openAcc(e) {
+  var parent = this.parentElement;
+  var article = this.nextElementSibling;
+  
+  if (!parent.classList.contains('js-active')) {
+    parent.classList.add('js-active');
+    article.style.maxHeight = article.scrollHeight + 'px';
+  }
+  else {
+    parent.classList.remove('js-active');
+    article.style.maxHeight = '0px';
+  }
+}
+
+//Use this as the callback if you would like no more than one dropdown to be open
+function openCurrAcc(e) {
+  for(var i = 0; i < headersAcc.length; i++) {
+    var parent = headersAcc[i].parentElement;
+    var article = headersAcc[i].nextElementSibling;
+
+    if (this === headersAcc[i] && !parent.classList.contains('js-active')) {
+      parent.classList.add('js-active');
+      article.style.maxHeight = article.scrollHeight + 'px';
+    }
+    else {
+      parent.classList.remove('js-active');
+      article.style.maxHeight = '0px';
+    }
+  }
+}
--- content/js/modules/close-alert.js
+++ content/js/modules/close-alert.js
@@ -1,23 +1,23 @@
 /* alert JS
    ========================================================================== */
 
-   class CloseAlert {
-    constructor(alert) {
-        this.alertClass = alert;
-        this.closeButton = alert.querySelector('[data-alert-close]');
-        this.init();
-    }
-
-    init() {
-      const parentClassScope = this;
-      this.closeButton.addEventListener('click', function(e){
-        parentClassScope.alertClass.remove();
-      });
-    }
+ class CloseAlert {
+  constructor(alert) {
+      this.alertClass = alert;
+      this.closeButton = alert.querySelector('[data-alert-close]');
+      this.init();
   }
 
-  const closableAlerts = document.querySelectorAll('[data-alert-closable]');
-
-  if (closableAlerts.length) {
-    [...closableAlerts].map((closableAlertsWithClick) => new CloseAlert(closableAlertsWithClick));
+  init() {
+    const parentClassScope = this;
+    this.closeButton.addEventListener('click', function(e){
+      parentClassScope.alertClass.remove();
+    });
   }
+}
+
+const closableAlerts = document.querySelectorAll('[data-alert-closable]');
+
+if (closableAlerts.length) {
+  [...closableAlerts].map((closableAlertsWithClick) => new CloseAlert(closableAlertsWithClick));
+}
