78619e6cd228a0ad237dd5f2334b3dcf50780026
Emily <emily.vandewalle@hotmail.com>
2022-01-18 18:46:51 +0100

    Add alternative filter view

---
+++ content/data/months.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+module.exports = months;
