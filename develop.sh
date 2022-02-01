7421ee3c46f2eff7b6ce74fdf4dc50de43bc64c3
Nyv. Violette Nys <nyv@smart.coop>
2022-02-01 15:10:35 +0100

    Disable path conversion
    When using Bash under Windows, there is an automatic path conversion
    which has to be switched off for our docker commands to run properly.
    
    Note that we disable the path conversion at the command level only by
    adding "MSYS_NO_PATHCONV=1" right before the docker command, on the very
    same line. It doesn't disable path conversion at shell (system) level.

--- develop.sh
+++ develop.sh
@@ -3,4 +3,8 @@
 # This runs the development server. Use ./init.sh beforehand to install the
 # dependencies to node_modules/.
 
-docker run -it -w /bedrock -v $(pwd):/bedrock -p 3000:3000 node:14.15.0-buster npm start
+if [[ $OSTYPE == "msys" ]]; then
+    MSYS_NO_PATHCONV=1 docker run -it -w /bedrock -v $(pwd):/bedrock -p 3000:3000 node:14.15.0-buster npm start
+else
+    docker run -it -w /bedrock -v $(pwd):/bedrock -p 3000:3000 node:14.15.0-buster npm start
+fi
--- init.sh
+++ init.sh
@@ -4,4 +4,8 @@
 # dependencies will be present in a local node_modules/ directory.
 # Use ./develop.sh to run the development server.
 
-docker run -it -w /bedrock -v $(pwd):/bedrock node:14.15.0-buster npm install
+if [[ $OSTYPE == "msys" ]]; then
+    MSYS_NO_PATHCONV=1 docker run -it -w /bedrock -v $(pwd):/bedrock node:14.15.0-buster npm install
+else
+    docker run -it -w /bedrock -v $(pwd):/bedrock node:14.15.0-buster npm install
+fi
