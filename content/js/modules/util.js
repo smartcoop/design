ab3c889c8fefe25c8c18a934a0a242fa9be8213c
marcelpirnay <110391411+marcelpirnay@users.noreply.github.com>
2022-12-05 16:44:15 +0100

    Curu v2 (#249)
    CURU Phase 1 - affichage.
    
    Vérifier si l'affichage correspond à ceci:
    https://www.figma.com/proto/Xg7VlIioBRfEGSN72297H4/Account?page-id=219%3A1721&node-id=602%3A10895&viewport=-319%2C464%2C0.57&scaling=min-zoom&starting-point-node-id=602%3A11962
    
    Co-authored-by: Pirnay Marcel <pirnaymarcel@Mac-mini-de-Pirnay.local>

--- content/js/modules/util.js
+++ content/js/modules/util.js
@@ -20,3 +20,87 @@ const isClickOutside = (event, elements) => {
 };
 
 export { isClickOutside };
+
+class SwitchViewButton {
+  constructor(els) {
+      this.els = els;
+      this.target__el = document.querySelector('.' + this.els[0].getAttribute('data-switch-target') );
+
+      this.init();
+  }
+
+  init() {
+      this.els.forEach((el, i) => {
+          // Attach events
+          el.addEventListener("click", (e) => {
+              if(el.getAttribute('data-toggle') === "disabled"){
+                  this.toggleAttribute(this.target__el, "disabled");
+              }else{
+                  e.preventDefault();
+                  this.toggleClass(this.target__el, ( el.getAttribute('data-switch-target') + '--' + el.getAttribute('data-toggle')));
+                  this.els.forEach((il) => {
+                      //- Toggle class for other buttons around
+                      this.toggleClass(il, "c-button--secondary");
+                      this.toggleClass(il, "c-button--primary");
+                  });
+              }
+
+          });
+      });
+     
+  }
+
+  toggleClass(target, toggleClass){
+      target.classList.toggle(toggleClass);
+  }
+  
+  toggleAttribute(target, attribute){
+      if (target.hasAttribute(attribute)) {
+          target.removeAttribute(attribute) 
+      }else{
+          target.setAttribute(attribute, "");
+      }
+  }
+
+}
+
+const switchViewButtons = document.querySelectorAll("[data-toggle]");
+
+if (switchViewButtons.length) {
+  new SwitchViewButton(switchViewButtons);
+}
+
+class ScrollToLink {
+  constructor(els) {
+      this.els = els;
+      this.target__el = document.querySelector('.' + this.els[0].getAttribute('data-scroll-target-id') );
+      console.log(els);
+      this.init();
+  }
+
+  init() {
+      this.els.forEach((el, i) => {
+          // Attach events
+          el.addEventListener("click", (e) => {
+            e.preventDefault();
+            var scrollToElement = document.querySelector('#' + el.getAttribute("data-scroll-target-id"));
+            this.scrollTo(scrollToElement)
+          });
+      });
+     
+  }
+
+  scrollTo(element) {
+    window.scrollTo({
+        top:  element.offsetTop,
+        behavior: "smooth"
+    });
+  }
+
+}
+
+const ScrollToLinks = document.querySelectorAll("[data-scroll-to]");
+
+if (ScrollToLinks.length) {
+  new ScrollToLink(ScrollToLinks);
+}
